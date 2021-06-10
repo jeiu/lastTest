67000ae17470375907eafc0851866201c757788e
pjy0509 <80870882+pjy0509@users.noreply.github.com>
2021-06-10 15:17:29 +0900

    if FileNotFoundException, create defualt json
    create defualt json

--- Json.java
+++ Json.java
@@ -4,45 +4,67 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /*
  * Date			'21.06.10 11:59
- * How			박준영	로컬 json파일 불러오는 method, json파일 저장하는 method
+ * How			박준영	1.0.0 로컬 json파일 불러오는 method, json파일 저장하는 method
+ * 						1.1.0 json파일을 저장하는 method 삭제
+ * 						2.0.0 초기 json파일을 생성하는 method 생성
  * Author		박준영
- * Version		1.1.0
+ * Version		2.0.0
  */
 
 public class Json {
 	CarEntranceExit cee = new CarEntranceExit();
 	
-	public JSONObject jsonLoad() {
+	
+	static JSONObject jsonLoad() {
 		JSONParser parser = new JSONParser();
 		JSONObject parkingLot = new JSONObject();
-		
-		// Load past json file
+		// 저장된 Json 파일 불러오기
 		try {
-			Object savedJson = parser.parse(new FileReader("c:\\parkingLot.json"));
+			Object savedJson = parser.parse(new FileReader("C:\\Users\\admin\\parkingLot.json"));
 			parkingLot = (JSONObject)savedJson;
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			try {
-				// If file not founded save empty file
-				FileWriter file = new FileWriter("c:\\parkingLot.json");
-				file.write(parkingLot.toJSONString());
-				file.flush();
-				file.close();
-			} catch (IOException f) {
-				f.printStackTrace();
-			}
+			System.out.println("초기 Json 파일을 생성합니다.");
+			jsonDefualt();
+			// e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
 		return parkingLot;
 	}
+	
+	// 초기 json 파일을 생성하는 method
+	static void jsonDefualt() {
+		JSONObject parkingLot = new JSONObject();
+		JSONArray parkingLotNum = new JSONArray();
+		for (int i = 0 ; i < 60 ; i++) {
+			parkingLotNum = new JSONArray();
+			parkingLotNum.add("");
+			parkingLotNum.add("");
+			parkingLot.put((i + 1), parkingLotNum);
+		}
+		// Json 데이터 C드라이브에 저장
+		try {
+			FileWriter file = new FileWriter("C:\\Users\\admin\\parkingLot.json");
+			file.write(parkingLot.toJSONString());
+			file.flush();
+			file.close();
+		} catch (IOException e) {
+			// 저장 실패시 에러문 출력 후 method 종료
+			System.out.println("초기 Json 파일 생성을 실패했습니다.");
+			e.printStackTrace();
+			System.exit(0);
+		}
+		System.out.println("초기 Json 파일 생성을 성공했습니다.");
+	}
 }
+
